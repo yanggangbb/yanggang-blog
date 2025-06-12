732ffe94868fadd092697276fe9a45a7bcd88065
박지후 <yanggang0424@gmail.com>
2025-06-12 20:23:35 +0900

    fix: footer 내용 변경

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -3,7 +3,8 @@ export default function Footer() {
     <footer className='bg-gray-100 py-4'>
       <div className='max-w-6xl mx-auto px-4'>
         <p className='text-center text-sm'>
-          &copy; {new Date().getFullYear()} MDX Blog. All rights reserved.
+          &copy; {new Date().getFullYear()} Yanggang's Blog. All rights
+          reserved.
         </p>
       </div>
     </footer>
