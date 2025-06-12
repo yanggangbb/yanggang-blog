eeccfb382b8463b2044513277e1ca211c2eb704d
박지후 <yanggang0424@gmail.com>
2025-06-12 20:20:13 +0900

    fix: header Blog명 변경경

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className='sticky top-0 z-10 bg-white/30 backdrop-blur-xl h-[70px] items-center flex px-4 border-b border-black/10'>
       <Link href='/' className='text-2xl font-bold'>
-        MDX Blog
+        Yanggang's Blog
       </Link>
     </header>
   );
