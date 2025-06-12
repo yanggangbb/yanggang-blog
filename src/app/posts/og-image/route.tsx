a54ef9743473d41211c93a3608f3a33e78409038
박지후 <yanggang0424@gmail.com>
2025-06-12 20:04:32 +0900

    fix: route font size 변경

--- src/app/posts/og-image/route.tsx
+++ src/app/posts/og-image/route.tsx
@@ -8,7 +8,7 @@ export function GET(request: Request) {
     (
       <div tw='flex flex-col w-full h-full items-center justify-center bg-white'>
         <div tw='flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-16'>
-          <h2 tw='flex flex-col text-6xl font-bold tracking-tight text-left'>
+          <h2 tw='flex flex-col text-8xl font-bold tracking-tight text-left'>
             {title}
           </h2>
         </div>
